39a781964a49081c8f8d248a54c0d211fa52f6cc
Juanse <juanseguzman@unicauca.edu.co>
2019-06-16 11:15:08 -0500

    LoadScene

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+    public void loadScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
