bd3add491fe080603ad4102dc1f47a21e41fc8d7
yeisonms <38954986+yeisonms@users.noreply.github.com>
2019-07-11 11:22:01 -0500

    Maps,(err)

--- Assets/Resources/Script/urlisf.cs
+++ Assets/Resources/Script/urlisf.cs
@@ -18,7 +18,8 @@ public class urlisf : MonoBehaviour
 
     public void linkisf()
     {
-        Application.OpenURL("http://www.semanasantapopayan.com/index.php?section=104");
+        //Application.OpenURL("http://www.semanasantapopayan.com/index.php?section=104");
+        Application.OpenURL("https://www.google.com/maps/place/Iglesia+de+San+Francisco/@2.4435844,-76.6105873,18.09z/data=!4m5!3m4!1s0x8e300310438eaac9:0x473fe8cc717e420f!8m2!3d2.4431634!4d-76.6083986");
     }
 
 
---
+++ Assets/Resources/Script/urlubiisf.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class urlubiisf : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void linkubiisf()
+    {
+        Application.OpenURL("https://www.google.com/maps/place/Iglesia+de+San+Francisco/@2.443372,-76.6093589,18.25z/data=!4m5!3m4!1s0x8e300310438eaac9:0x473fe8cc717e420f!8m2!3d2.4431634!4d-76.6083986");
+    }
+}
