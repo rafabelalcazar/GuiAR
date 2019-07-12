b006d6fc84245dfa7171d9d7491dd89de5b5ece4
yeisonms <38954986+yeisonms@users.noreply.github.com>
2019-07-12 11:10:51 -0500

    funcional apk

---
+++ Assets/Resources/Script/urlubiegy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class urlubiegy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void linkubiisf()
+    {
+        Application.OpenURL("https://www.google.com/maps/place/Centro+Comercial+Terra+Plaza/@2.4868135,-76.5625747,17z/data=!3m1!4b1!4m5!3m4!1s0x8e3004177e274495:0x5df1054aaf95cad2!8m2!3d2.4868081!4d-76.560386");
+    }
+}
---
+++ Assets/Resources/Script/urlubitrl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class urlubitrl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void linkubiisf()
+    {
+        Application.OpenURL("https://www.google.com/maps/search/torre+del+relog+popayan/@2.4426168,-76.6077168,18z");
+    }
+
+}
